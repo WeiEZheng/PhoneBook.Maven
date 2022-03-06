2ca0f395153da323016015bd0855bdf7e322c211
Wei <wei@Zipcoders-MacBook-Pro.local>
2022-03-06 00:59:07 -0500

    finish all based on readme but cannot get it to return an ordered list of name for getallcontacts

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,11 +1,8 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 //import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
+
 
 /**
  * Created by leon on 1/23/18.
@@ -16,39 +13,53 @@ public class PhoneBook {
     private final Map<String, List<String>> phonebook;
 
     public PhoneBook(Map<String, List<String>> map) {
-        this.phonebook = null;
+        phonebook = map;
     }
 
     public PhoneBook() {
-        this(null);
+        this(new HashMap<>());
     }
 
     public void add(String name, String phoneNumber) {
+        List <String> phoneNum = phonebook.get(name);
+        if (phoneNum == null)
+            phoneNum = new ArrayList<>();
+        phoneNum.add(phoneNumber);
+        phonebook.put(name,phoneNum);
     }
 
     public void addAll(String name, String... phoneNumbers) {
+        for (String s : phoneNumbers) {
+            this.add(name,s);
+        }
     }
 
     public void remove(String name) {
+        phonebook.remove(name);
     }
 
     public Boolean hasEntry(String name) {
-        return null;
+        return phonebook.get(name)!=null;
     }
 
     public List<String> lookup(String name) {
-        return null;
+        return phonebook.get(name);
     }
 
     public String reverseLookup(String phoneNumber)  {
-        return null;
+        for (String s:this.getAllContactNames()){
+            if (lookup(s).toString().contains(phoneNumber))
+                return s;
+        }
+        return null; //if it is not in the map returns null
     }
 
     public List<String> getAllContactNames() {
-        return null;
+        return new ArrayList<>(phonebook.keySet());
+        //keyset is not ordered - Not sure how to fix or if there is other method of doing so
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return phonebook;
     }
 }
